282bada563b24d3b750c996c21ae61fc3d9c9202
garamv20 <garamv20@gmail.com>
2022-03-13 23:12:03 +0900

    restproject init

---
+++ src/main/java/com/cocoball/kokoatalkrest/KokoaTalkRestApplication.java
@@ -0,0 +1,13 @@
+package com.cocoball.kokoatalkrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokoaTalkRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokoaTalkRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cocoball/kokoatalkrest/KokoaTalkRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocoball.kokoatalkrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokoaTalkRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
